bc60c5247e43d79743948eecb8d53572e5fbea47
yhx9527 <540230904@qq.com>
2021-07-24 11:20:14 +0800

    add(goog2esm): config

---
+++ goog2esm.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: 'Blockly',
+    pendingDirs: [
+        'blocks_common',
+        'blocks_horizontal',
+        'blocks_vertical',
+        'core',
+        'msg',
+    ],
+}
